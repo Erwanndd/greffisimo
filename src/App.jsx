bd2e580fb6a1219643c6fe3affa3456f54934004
Erwanndd <erwann.del.do@gmail.com>
2025-10-26 15:36:06 +0100

    New main page video
    New main page video

--- src/App.jsx
+++ src/App.jsx
@@ -173,7 +173,6 @@ const HomePage = () => {
                     width="100%"
                     height="100%"
                     src="https://www.youtube.com/embed/dQw4w9WgXcQ"
-                    src="videos/greffissimo-course.mp4"
                     title="Greffissimo - Présentation vidéo"
                     frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
